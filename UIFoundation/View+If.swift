869e89b070d3f70527ffe5da2a48777f25d9cb85
Porter McGary <56183563+mcgaryp@users.noreply.github.com>
2024-04-28 21:19:28 -0600

    make it public

--- UIFoundation/View+If.swift
+++ UIFoundation/View+If.swift
@@ -13,7 +13,7 @@ extension View {
     ///   - condition: The condition to evaluate.
     ///   - transform: The transform to apply to the source `View`.
     /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
-    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+    @ViewBuilder public func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
         if condition {
             transform(self)
         } else {
@@ -26,7 +26,7 @@ extension View {
     ///   - condition: The conditional value.
     ///   - transform: The transform to apply to the source `View`.
     /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
-    @ViewBuilder func `if`<Content: View, Arg>(_ value: Arg?, transform: (Self, Arg) -> Content) -> some View {
+    @ViewBuilder public func `if`<Content: View, Arg>(_ value: Arg?, transform: (Self, Arg) -> Content) -> some View {
         if let value {
             transform(self, value)
         } else {
